b5e18c7e4c9ac1d2433d657cabafb82865e43003
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-06-28 23:02:31 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine().trim();
+        boolean isPalindrome = true;
+        int length = input.length();
+
+        for (int i = 0; i < length / 2; i++) {
+            if (input.charAt(i) != input.charAt(length - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
