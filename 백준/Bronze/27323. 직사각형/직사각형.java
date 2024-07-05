3529527aeddd22231ab5a7cda4616fa9a7ae7107
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-07-06 01:07:27 +0900

    [Bronze V] Title: 직사각형, Time: 116 ms, Memory: 14560 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        int area = A * B;
+
+        bw.write(area + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
